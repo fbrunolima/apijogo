cc39a3896f96612c8835cbefb961c1aec50a34e5
Felipe Bruno <ti.fbrunolima@gmail.com>
2021-09-08 21:04:49 -0300

    Projeto Lista de Jogos

---
+++ Services/JogoService.cs
@@ -0,0 +1,24 @@
+﻿using ApiGame.InputModel;
+using ApiGame.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiGame.Services
+{
+    public interface JogoService : IDisposable
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+
+        Task<JogoViewModel> Obter(Guid id);
+
+        Task<JogoViewModel> Inserir(JogoInputModel jogo);
+
+        Task Atualizar(Guid id, JogoInputModel jogo);
+
+        Task Atualizar(Guid id, double preco);
+
+        Task Remover(Guid id)
+    }
+}
